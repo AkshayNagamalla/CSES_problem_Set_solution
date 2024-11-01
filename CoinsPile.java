8e3d7dea0a6e41d7a4508f7e7bbbcf4ca965b13b
Akshay Nagamalla <akshaynagamalla23@gmail.com>
2024-11-01 10:56:29 +0530

    1754 Coins pile

---
+++ CoinsPile.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+/*
+ * link : https://cses.fi/problemset/task/1754
+ */
+public class CoinsPile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        PrintWriter out = new PrintWriter(System.out);
+
+        while (t-- != 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if (a > 2 * b || b > 2 * a) {
+                out.println("NO");
+            } else if ((a + b) % 3 == 0) {
+                out.println("YES");
+            } else {
+                out.println("NO");
+            }
+        }
+        out.flush();
+    }
+}
